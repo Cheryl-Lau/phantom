d3dbf5b083c10b81bdd21d31c863c95c81036851
Cheryl-Lau <lausiching@gmail.com>
2023-05-10 05:49:37 +0100

    added set starting dumpfile

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -3,14 +3,25 @@
 # Rerun a phantom simulation
 #
 read -p 'Filename: ' jobname
-length=${#jobname}
-
-if [ $length == 0 ]; then
+length_job=${#jobname}
+if [ $length_job == 0 ]; then
    exit 2
 fi
 
-rm p*
-rm $jobname*
+read -p 'Dumpfile: ' dumpfile
+length_dump=${#dumpfile}
+if [ $length_dump == 0 ]; then
+   starting_dump=$jobname'_00000.tmp'
+else
+   starting_dump=$dumpfile
+fi
+
+rm phantom*
+if [ $length_dump == 0 ]; then
+   rm $jobname*
+else
+   rm $jobname.*
+fi
 make cleanall
 
 make; make setup
@@ -19,6 +30,8 @@ if [[ -f 'phantom' ]] || [[ -f 'phantomsetup' ]]; then
 	./phantomsetup $jobname
 	./phantomsetup $jobname
 	if [[ -f $jobname'.in' ]]; then
-	./phantom $jobname.in
+      starting_dump_default=$jobname'_00000.tmp'
+      sed -i "s/$starting_dump_default/$starting_dump/g" $jobname'.in'
+	   ./phantom $jobname.in
 	fi
 fi
