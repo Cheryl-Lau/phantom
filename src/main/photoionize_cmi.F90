b70156321e626c0f9c01516b24925be1c606a1e6
Cheryl-Lau <lausiching@gmail.com>
2023-12-11 16:16:43 +0000

    changed src loc

--- src/main/photoionize_cmi.F90
+++ src/main/photoionize_cmi.F90
@@ -82,7 +82,7 @@ module photoionize_cmi
  !- or
  ! Manually set location, starting/ending time and ionizing photon flux [cgs units] of sources
  integer, public, parameter :: nsetphotosrc = 1
- real,    public :: xyztq_setphotosrc_cgs(6,nsetphotosrc) = reshape((/ 0., 0., 0., 0.,1E60,1E50 /),&
+ real,    public :: xyztq_setphotosrc_cgs(6,nsetphotosrc) = reshape((/ 4.0169e+18, 5.8504e+18, 1.3752e+18, 0.,1E60,1E50 /),&
                                                                     shape=(/6,nsetphotosrc/))
  ! Monte Carlo simulation settings
  integer, public :: nphoton    = 1E6
@@ -107,7 +107,7 @@ module photoionize_cmi
 
  ! Options for cropping simulation domain being passed to CMI (to avoid segfault; use with caution)
  logical, public :: crop_domain  = .true.
- real,    public :: crop_fac     = 3.0          ! bounds = crop_fac*rcut_opennode (>1)
+ real,    public :: crop_fac     = 2.5          ! bounds = crop_fac*rcut_opennode (>1)
 
  ! Options for heating/cooling
  real,    public :: temp_hii     = 1E4          ! K
