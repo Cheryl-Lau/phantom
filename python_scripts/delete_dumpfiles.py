4718b4f64625476bd74b227f162a88e0d9174dc8
Cheryl-Lau <lausiching@gmail.com>
2024-02-01 21:00:28 +0000

    clear only the early dumpfiles

--- python_scripts/delete_dumpfiles.py
+++ python_scripts/delete_dumpfiles.py
@@ -4,13 +4,21 @@ For deleting dumpfiles
 """
 
 import os 
+import re
+
+dump_prefix = "sphere_"
+idump_lastdumptocheck = 1000
 
 pwd = os.getcwd()
 for file in os.listdir(pwd):
-    if file.startswith("sphere_"):
-        print(file)
-        
-        ends = ( "1","2","3","4","5","6","7","8","9")
+    if file.startswith(dump_prefix.strip()):
+        idump = re.findall(r'\d+',file)
+        idump = int(idump[0])
+        print(file,idump)
 
-        if file.endswith(ends):
-            os.remove(file)
+        if (idump < idump_lastdumptocheck):
+
+            ends = ( "1","2","3","4","5","6","7","8","9")
+
+            if file.endswith(ends):
+                os.remove(file)
