012bb37c14066d94321d0ccfa82142c39391881b
Cheryl-Lau <lausiching@gmail.com>
2024-02-01 21:33:48 +0000

    treated file prefix as dumpfile number bug fix

--- python_scripts/delete_dumpfiles.py
+++ python_scripts/delete_dumpfiles.py
@@ -7,18 +7,25 @@ import os
 import re
 
 dump_prefix = "sphere_"
-idump_lastdumptocheck = 1000
+idump_lastdumptocheck = 20
+
+print('dump prefix: ',dump_prefix)
+print('last idump to delete: ',idump_lastdumptocheck)
+
+proceed = input('Proceed [y/n]? ')
+if (proceed.strip() != 'y'):
+    quit() 
 
 pwd = os.getcwd()
 for file in os.listdir(pwd):
     if file.startswith(dump_prefix.strip()):
-        idump = re.findall(r'\d+',file)
+        idump = re.findall(r'\d+',file.replace(dump_prefix, ''))
         idump = int(idump[0])
-        print(file,idump)
 
         if (idump < idump_lastdumptocheck):
 
             ends = ( "1","2","3","4","5","6","7","8","9")
 
             if file.endswith(ends):
+                print('Deleting ',file)
                 os.remove(file)
