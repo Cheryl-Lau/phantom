effc5621503558b1320a7e5be6731825aff6cc4e
Cheryl-Lau <lausiching@gmail.com>
2024-05-27 20:48:28 +0100

    abs v and changed location

--- src/utils/analysis_detector_near_sn.f90
+++ src/utils/analysis_detector_near_sn.f90
@@ -24,7 +24,7 @@ module analysis
 
  private
 
- real    :: xyz_target(3) = (/ 10., 0., 0. /)  ! Position of detector
+ real    :: xyz_target(3) = (/ 7., 0., 0. /)  ! Position of detector
 
 contains
 
