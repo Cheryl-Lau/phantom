fbaf25458a12775d6bc8bb78412a258879b34c96
Cheryl-Lau <lausiching@gmail.com>
2024-08-21 04:24:05 +0100

    revert

--- src/main/evolve.F90
+++ src/main/evolve.F90
@@ -281,14 +281,12 @@ subroutine evol(infile,logfile,evfile,dumpfile)
 
 
 #ifdef INJECT_PARTICLES
-   if (istepfrac+1==2**nbinmax) then 
-      !
-      ! injection of new particles into simulation
-      !
-      npart_old=npart
-      call inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype,dtinject)
-      call update_injected_particles(npart_old,npart,istepfrac,nbinmax,time,dtmax,dt,dtinject)
-   endif 
+   !
+   ! injection of new particles into simulation
+   !
+   npart_old=npart
+   call inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype,dtinject)
+   call update_injected_particles(npart_old,npart,istepfrac,nbinmax,time,dtmax,dt,dtinject)
 #endif
 
     dtlast = dt
