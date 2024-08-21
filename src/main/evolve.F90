b9d90a5f6c2b282b34965b27a414b1532d436d30
Cheryl-Lau <lausiching@gmail.com>
2024-08-21 03:52:20 +0100

    inject only at full step

--- src/main/evolve.F90
+++ src/main/evolve.F90
@@ -280,16 +280,15 @@ subroutine evol(infile,logfile,evfile,dumpfile)
     endif
 
 
-
 #ifdef INJECT_PARTICLES
-    if (istepfrac==2**nbinmax) then
-       !
-       ! injection of new particles into simulation
-       !
-       npart_old=npart
-       call inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype,dtinject)
-       call update_injected_particles(npart_old,npart,istepfrac,nbinmax,time,dtmax,dt,dtinject)
-    endif 
+   if (istepfrac+1==2**nbinmax) then 
+      !
+      ! injection of new particles into simulation
+      !
+      npart_old=npart
+      call inject_particles(time,dtlast,xyzh,vxyzu,xyzmh_ptmass,vxyz_ptmass,npart,npartoftype,dtinject)
+      call update_injected_particles(npart_old,npart,istepfrac,nbinmax,time,dtmax,dt,dtinject)
+   endif 
 #endif
 
     dtlast = dt
