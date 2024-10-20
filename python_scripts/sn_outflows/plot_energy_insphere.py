081caa251124a1e9545910232bd7956651a9db27
Cheryl-Lau <lausiching@gmail.com>
2024-10-20 22:05:36 +0100

    updated plot scripts

--- python_scripts/sn_outflows/plot_energy_insphere.py
+++ python_scripts/sn_outflows/plot_energy_insphere.py
@@ -9,136 +9,94 @@ import matplotlib.pyplot as plt
 import glob 
 import os 
 
-df = pd.DataFrame(columns=('chnl', 'time', 'radius', 'ekin','etherm','etot'))
 
 Myr = 1E6*365*24*60*60 
 pc = 3.086E+18
 
-for filename in glob.glob('free_field/energy_insphere_nocloud_bigenv_*.dat'):  # over_time 
-    time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
-    radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
-    df00 = pd.DataFrame({'chnl':[0.0]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
-    df = pd.concat([df,df00],ignore_index=True)
-
-for filename in glob.glob('semi_confined01/energy_insphere_withhii_chnl01_*.dat'): 
-    time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
-    radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
-    df01 = pd.DataFrame({'chnl':[0.1]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
-    df = pd.concat([df,df01],ignore_index=True)
-
-#for filename in glob.glob('semi_confined02/energy_insphere_withhii_chnl02_*.dat'): 
-#    time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
-#    radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
-#    df02 = pd.DataFrame({'chnl':[0.2]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
-#    df = pd.concat([df,df02],ignore_index=True)
-
-for filename in glob.glob('semi_confined03/energy_insphere_withhii_chnl03_*.dat'): 
-    time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
-    radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
-    df03 = pd.DataFrame({'chnl':[0.3]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
-    df = pd.concat([df,df03],ignore_index=True)
-
-print(df)
 
 
+def read_input_combineframe():
 
-fig, (ax1,ax2,ax3) = plt.subplots(nrows=3, sharex=True, subplot_kw=dict(frameon=True), figsize=(6,10), dpi=300)
-plt.subplots_adjust(hspace=.0)
+    df = pd.DataFrame(columns=('chnl', 'time', 'radius', 'ekin','etherm','etot'))
 
+    for filename in glob.glob('free_field/energy_insphere_nocloud_bigenv_*.dat'):  # over_time 
+        time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
+        radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
+        df00 = pd.DataFrame({'chnl':[0.0]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
+        df = pd.concat([df,df00],ignore_index=True)
 
-# rad = 20 pc
-iselect = np.where(np.logical_and(df['radius'] == 20.0,df['chnl']==0.0))[0]
-time_r20_ff00 = df['time'].iloc[iselect].to_numpy()
-etot_r20_ff00 = df['etot'].iloc[iselect].to_numpy()
-iselect = np.where(np.logical_and(df['radius'] == 20.0,df['chnl']==0.1))[0]
-time_r20_cf01 = df['time'].iloc[iselect].to_numpy()
-etot_r20_cf01 = df['etot'].iloc[iselect].to_numpy()
-#iselect = np.where(np.logical_and(df['radius'] == 20.0,df['chnl']==0.2))[0]
-#time_r20_cf02 = df['time'].iloc[iselect].to_numpy()
-#etot_r20_cf02 = df['etot'].iloc[iselect].to_numpy()
-iselect = np.where(np.logical_and(df['radius'] == 20.0,df['chnl']==0.3))[0]
-time_r20_cf03 = df['time'].iloc[iselect].to_numpy()
-etot_r20_cf03 = df['etot'].iloc[iselect].to_numpy()
+    for filename in glob.glob('semi_confined01/energy_insphere_withhii_chnl01_*.dat'): 
+        time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
+        radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
+        df01 = pd.DataFrame({'chnl':[0.1]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
+        df = pd.concat([df,df01],ignore_index=True)
 
-time0_r20_ff = np.min(time_r20_ff00)   # sync to time when shock arrives rad 
-time0_r20_cf = np.min(time_r20_cf01)  
+    for filename in glob.glob('semi_confined02/energy_insphere_withhii_chnl02_*.dat'): 
+        time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
+        radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
+        df02 = pd.DataFrame({'chnl':[0.2]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
+        df = pd.concat([df,df02],ignore_index=True)
 
-ax1 = fig.add_subplot(311)
-ax1.scatter(time_r20_ff00-time0_r20_ff,etot_r20_ff00,s=1,color='darkblue',label='free-field')
-ax1.scatter(time_r20_cf01-time0_r20_cf,etot_r20_cf01,s=1,color='salmon',label='$\Omega = 0.1 \cdot 4 \pi$')
-#ax1.scatter(time_r20_cf02-time0_r20_cf,etot_r20_cf02,s=1,color='orangered',label='$\Omega = 0.2 \cdot 4 \pi$')
-ax1.scatter(time_r20_cf03-time0_r20_cf,etot_r20_cf03,s=1,color='firebrick',label='$\Omega = 0.3 \cdot 4 \pi$')
-ax1.set_yscale('log')
-ax1.set_ylim([3E49,1E52])
-ax1.set_xlabel('Time [Myr]')
-ax1.set_ylabel('Total energy [erg]')
-ax1.text(0.1,5E51,'radius = 20 pc')
-ax1.legend()
+    for filename in glob.glob('semi_confined03/energy_insphere_withhii_chnl03_*.dat'): 
+        time = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
+        radius, ke, te, tote = np.loadtxt(filename,skiprows=3,unpack=True)
+        df03 = pd.DataFrame({'chnl':[0.3]*len(radius),'time':time/Myr,'radius':radius/pc,'ekin':ke,'etherm':te,'etot':tote})
+        df = pd.concat([df,df03],ignore_index=True)
+
+    print(df)
+
+    return df
 
 
-# rad = 50 pc
-iselect = np.where(np.logical_and(df['radius'] == 50.0,df['chnl']==0.0))[0]
-time_r50_ff00 = df['time'].iloc[iselect].to_numpy()
-etot_r50_ff00 = df['etot'].iloc[iselect].to_numpy()
-iselect = np.where(np.logical_and(df['radius'] == 50.0,df['chnl']==0.1))[0]
-time_r50_cf01 = df['time'].iloc[iselect].to_numpy()
-etot_r50_cf01 = df['etot'].iloc[iselect].to_numpy()
-#iselect = np.where(np.logical_and(df['radius'] == 50.0,df['chnl']==0.2))[0]
-#time_r50_cf02 = df['time'].iloc[iselect].to_numpy()
-#etot_r50_cf02 = df['etot'].iloc[iselect].to_numpy()
-iselect = np.where(np.logical_and(df['radius'] == 50.0,df['chnl']==0.3))[0]
-time_r50_cf03 = df['time'].iloc[iselect].to_numpy()
-etot_r50_cf03 = df['etot'].iloc[iselect].to_numpy()
+def plot_energ_in_rad(ax,rad,df):
 
-time0_r50_ff = np.min(time_r50_ff00)   # sync to time when shock arrives rad 
-time0_r50_cf = np.min(time_r50_cf01)  
+    iselect = np.where(np.logical_and(df['radius'] == rad,df['chnl']==0.0))[0]
+    time_ff00 = df['time'].iloc[iselect].to_numpy()
+    etot_ff00 = df['etot'].iloc[iselect].to_numpy()
+    iselect = np.where(np.logical_and(df['radius'] == rad,df['chnl']==0.1))[0]
+    time_cf01 = df['time'].iloc[iselect].to_numpy()
+    etot_cf01 = df['etot'].iloc[iselect].to_numpy()
+    #iselect = np.where(np.logical_and(df['radius'] == rad,df['chnl']==0.2))[0]
+    #time_cf02 = df['time'].iloc[iselect].to_numpy()
+    #etot_cf02 = df['etot'].iloc[iselect].to_numpy()
+    iselect = np.where(np.logical_and(df['radius'] == rad,df['chnl']==0.3))[0]
+    time_cf03 = df['time'].iloc[iselect].to_numpy()
+    etot_cf03 = df['etot'].iloc[iselect].to_numpy()
 
-ax2 = fig.add_subplot(312)
-ax2.scatter(time_r50_ff00-time0_r50_ff,etot_r50_ff00,s=1,color='darkblue',label='free-field')
-ax2.scatter(time_r50_cf01-time0_r50_cf,etot_r50_cf01,s=1,color='salmon',label='$\Omega = 0.1 \cdot 4 \pi$')
-#ax2.scatter(time_r50_cf02-time0_r50_cf,etot_r50_cf02,s=1,color='orangered',label='$\Omega = 0.2 \cdot 4 \pi$')
-ax2.scatter(time_r50_cf03-time0_r50_cf,etot_r50_cf03,s=1,color='firebrick',label='$\Omega = 0.3 \cdot 4 \pi$')
-ax2.set_yscale('log')
-ax2.set_ylim([1E50,5E52])
-ax2.set_xlabel('Time [Myr]')
-ax2.set_ylabel('Total energy [erg]')
-ax2.text(0.1,2.5E52,'radius = 50 pc')
-ax2.legend()
+    time0_ff = np.min(time_ff00)   # sync to time when shock arrives rad 
+    time0_cf = np.min(time_cf01)  
+
+    ax.scatter(time_ff00-time0_ff,etot_ff00,s=1,color='darkblue',label='free-field')
+    ax.scatter(time_cf01-time0_cf,etot_cf01,s=1,color='salmon',label='confined '+'$\Omega = 0.1 \cdot 4 \pi$')
+    #ax.scatter(time_cf02-time0_cf,etot_cf02,s=1,color='orangered',label='confined '+'$\Omega = 0.2 \cdot 4 \pi$')
+    ax.scatter(time_cf03-time0_cf,etot_cf03,s=1,color='firebrick',label='confined '+'$\Omega = 0.3 \cdot 4 \pi$')
+    ax.set_yscale('log')
+    ax.set_xlabel('time [Myr]')
+    ax.set_xlim([0,3.1])
+    ax.set_ylim([3e46,6e51])
+    ax.legend(loc='lower left',fontsize=9)
+    ax.text(1.8,1.5e51,'r < '+str(int(rad))+' pc')
+
+    ax.minorticks_on()
+    ax.xaxis.set_tick_params(which='minor')
+
+    return 
 
 
-# rad = 100 pc
-iselect = np.where(np.logical_and(df['radius'] == 100.0,df['chnl']==0.0))[0]
-time_r100_ff00 = df['time'].iloc[iselect].to_numpy()
-etot_r100_ff00 = df['etot'].iloc[iselect].to_numpy()
-iselect = np.where(np.logical_and(df['radius'] == 100.0,df['chnl']==0.1))[0]
-time_r100_cf01 = df['time'].iloc[iselect].to_numpy()
-etot_r100_cf01 = df['etot'].iloc[iselect].to_numpy()
-#iselect = np.where(np.logical_and(df['radius'] == 100.0,df['chnl']==0.2))[0]
-#time_r100_cf02 = df['time'].iloc[iselect].to_numpy()
-#etot_r100_cf02 = df['etot'].iloc[iselect].to_numpy()
-iselect = np.where(np.logical_and(df['radius'] == 100.0,df['chnl']==0.3))[0]
-time_r100_cf03 = df['time'].iloc[iselect].to_numpy()
-etot_r100_cf03 = df['etot'].iloc[iselect].to_numpy()
+df = read_input_combineframe()
 
-time0_r100_ff = np.min(time_r100_ff00)   # sync to time when shock arrives rad 
-time0_r100_cf = np.min(time_r100_cf01)  
+fig, (ax1,ax2,ax3,ax4) = plt.subplots(ncols=4, sharey=True, subplot_kw=dict(frameon=True), figsize=(10,3), dpi=300)
 
-ax3 = fig.add_subplot(313)
-ax3.scatter(time_r100_ff00-time0_r100_ff,etot_r100_ff00,s=1,color='darkblue',label='free-field')
-ax3.scatter(time_r100_cf01-time0_r100_cf,etot_r100_cf01,s=1,color='salmon',label='$\Omega = 0.1 \cdot 4 \pi$')
-#ax3.scatter(time_r100_cf02-time0_r100_cf,etot_r100_cf02,s=1,color='orangered',label='$\Omega = 0.2 \cdot 4 \pi$')
-ax3.scatter(time_r100_cf03-time0_r100_cf,etot_r100_cf03,s=1,color='firebrick',label='$\Omega = 0.3 \cdot 4 \pi$')
-ax3.set_yscale('log')
-ax3.set_ylim([3E50,5E52])
-ax3.set_xlabel('Time [Myr]')
-ax3.set_ylabel('Total energy [erg]')
-ax3.text(0.1,2.5E52,'radius = 100 pc')
-ax3.legend()
+plot_energ_in_rad(ax1,20.0,df)
+plot_energ_in_rad(ax2,50.0,df)
+plot_energ_in_rad(ax3,80.0,df)
+plot_energ_in_rad(ax4,100.0,df)
+
+ax1.set_ylabel('total energy [erg]')
+fig.tight_layout(pad=1.0)
 
 
 plt.savefig('energy_insphere.png')
-plt.show()
-
 
 
 
--- python_scripts/sn_outflows/plot_gasflow_compare.py
+++ python_scripts/sn_outflows/plot_gasflow_compare.py
@@ -6,142 +6,84 @@ import matplotlib.pyplot as plt
 import glob 
 import os 
 
+pc = 3.086E+18
+Myr = 1E6*365*24*60*60
 
-duration = 1.5E+13
-
-#
-# Confined case - Time evol of properties at target point 
-#
-time_evol_cav = []
-rho_evol_cav = []
-velx_evol_cav = []
-thermpr_evol_cav = []
-rampr_evol_cav = []
-
-for filename in glob.glob('gasflow_cavitysn_*.dat'):  # si units 
-    file = open(filename)
-    for iline,line in enumerate(file):
-        line = line.split()
-        if (iline == 1):
-            time = line[0]
-            time_evol_cav.append(float(time))
-        elif (iline == 3): 
-            rho = line[0]
-            velx = line[1]
-            thermpr = line[2]
-            rampr = line[3]
-            rho_evol_cav.append(float(rho))
-            velx_evol_cav.append(float(velx))
-            thermpr_evol_cav.append(float(thermpr))
-            rampr_evol_cav.append(float(rampr))
-
-# Sync at point when sn shock arrives 
-t0_cav = 3.411E+13
-icrop = np.where(np.logical_and(np.array(time_evol_cav) > t0_cav, np.array(time_evol_cav) < t0_cav+duration))[0]
-
-time_evol_cav = np.take(time_evol_cav,icrop) - t0_cav
-rho_evol_cav = np.take(rho_evol_cav,icrop)
-velx_evol_cav = np.take(velx_evol_cav,icrop)
-thermpr_evol_cav = np.take(thermpr_evol_cav,icrop)
-rampr_evol_cav = np.take(rampr_evol_cav,icrop)
+# Sync to shock arrival time 
+t0_shock_cf = 0.4E+14/Myr
+t0_shock_ff = 0.03E+14/Myr
 
 
-#
-# Free-field case - Time evol of properties at target point 
-#
-time_evol_ff = []
-rho_evol_ff = []
-velx_evol_ff = []
-thermpr_evol_ff = []
-rampr_evol_ff = []
+def plot_gas_detec(ax1,ax2,ax3,filepath,labelstr,colourstr,t0_shock):
 
-for filename in glob.glob('gasflow_ffsn_*.dat'):  # si units 
-    file = open(filename)
-    for iline,line in enumerate(file):
-        line = line.split()
-        if (iline == 1):
-            time = line[0]
-            time_evol_ff.append(float(time))
-        elif (iline == 3): 
-            rho = line[0]
-            velx = line[1]
-            thermpr = line[2]
-            rampr = line[3]
-            rho_evol_ff.append(float(rho))
-            velx_evol_ff.append(float(velx))
-            thermpr_evol_ff.append(float(thermpr))
-            rampr_evol_ff.append(float(rampr))
+    firstcall = True
+    for filename in glob.glob(filepath):  
 
-# Sync at point when sn shock arrives 
-t0_ff = 6.2E+11 
-icrop = np.where(np.logical_and(np.array(time_evol_ff) > t0_ff, np.array(time_evol_ff) < t0_ff+duration))[0]
+        time_cgs,x_cgs,y_cgs,z_cgs = np.loadtxt(filename,skiprows=1,max_rows=1,unpack=True)
+        time_Myr = time_cgs/Myr - t0_shock
 
-time_evol_ff = np.take(time_evol_ff,icrop) - t0_ff
-rho_evol_ff = np.take(rho_evol_ff,icrop)
-velx_evol_ff = np.take(velx_evol_ff,icrop)
-thermpr_evol_ff = np.take(thermpr_evol_ff,icrop)
-rampr_evol_ff = np.take(rampr_evol_ff,icrop)
+        rho_cgs,vx_cgs,u_cgs,thermpr_cgs,rampr_cgs = np.loadtxt(filename,skiprows=3,max_rows=3,unpack=True)
+
+        if (firstcall==True):
+            ax1.scatter(time_Myr,vx_cgs,s=1,color=colourstr,label=labelstr)
+            ax2.scatter(time_Myr,thermpr_cgs,s=1,color=colourstr,label=labelstr)
+            ax3.scatter(time_Myr,rampr_cgs,s=1,color=colourstr,label=labelstr)
+            firstcall = False
+        else:
+            ax1.scatter(time_Myr,vx_cgs,s=1,color=colourstr)
+            ax2.scatter(time_Myr,thermpr_cgs,s=1,color=colourstr)
+            ax3.scatter(time_Myr,rampr_cgs,s=1,color=colourstr)
+
+    return 
+
+        
+def plot_analyt_model(ax1,ax2,ax3,filepath,labelstr,colourstr):
+
+    time_cgs,vx_cgs,thermpr_cgs,rampr_cgs = np.loadtxt(filepath,unpack=True)
+    time_Myr = time_cgs/Myr 
+
+    ax1.plot(time_Myr,vx_cgs,'--',color=colourstr,label=labelstr)
+    ax2.plot(time_Myr,thermpr_cgs,'--',color=colourstr,label=labelstr)
+    ax3.plot(time_Myr,rampr_cgs,'--',color=colourstr,label=labelstr)
+
+    return 
 
 
+fig,(ax1,ax2,ax3) = plt.subplots(nrows=3,sharex=False,subplot_kw=dict(frameon=True),figsize=(5,9),dpi=200)
+#plt.subplots_adjust(hspace=.0)
 
-'''
-fig1 = plt.figure(figsize=[7,5])
-ax1 = fig1.add_subplot(111)
-ax1.scatter(time_evol_cav,rho_evol_cav,s=1,color='red',label='partially-confined')
-ax1.scatter(time_evol_ff,rho_evol_ff,s=1,color='blue')
-ax1.set_xlabel('time [s]')
-ax1.set_ylabel('rho [kg m^-3]')
+plot_analyt_model(ax1,ax2,ax3,'semi_confined_model.txt','semi-confined model','salmon')
+plot_analyt_model(ax1,ax2,ax3,'free_field_model.txt','free-field model','royalblue')
+
+plot_gas_detec(ax1,ax2,ax3,'semi_confined01/gasflow_withhii_chnl01_*.dat','semi-confined','red',t0_shock_cf)
+plot_gas_detec(ax1,ax2,ax3,'free_field/gasflow_nocloud_bigenv_*.dat','free-field','blue',t0_shock_ff)
+
+ax1.set_xlabel('time [Myr]')
+ax1.set_ylabel('velocity [cm $\mathrm{s^{-1}}$]')
 ax1.set_yscale('log')
-ax1.legend()
-'''
+ax1.set_xlim([0,3.1])
+ax1.set_ylim([1E5,6E7])
+ax1.legend(loc='upper right',fontsize=8)
 
-fig2 = plt.figure(figsize=[7,5])
-ax2 = fig2.add_subplot(111)
-ax2.scatter(time_evol_cav,velx_evol_cav,s=1,color='red',label='partially-confined')
-ax2.scatter(time_evol_ff,velx_evol_ff,s=1,color='blue',label='free-field')
-ax2.set_xlabel('time [s]')
-ax2.set_ylabel('v_x [m s^-1]')
+ax2.set_xlabel('time [Myr]')
+ax2.set_ylabel('thermal pressure [g $\mathrm{cm^{-1}}$ $\mathrm{s^{-2}}$]')
 ax2.set_yscale('log')
-ax2.set_ylim([5E3,6E5])
-ax2.legend()
-fig2.savefig('sim_vx.png',dpi=200)
+ax2.set_xlim([0,3.1])
+ax2.set_ylim([2E-13,1E-8])
+ax2.legend(loc='upper right',fontsize=8)
 
-fig3 = plt.figure(figsize=[7,5])
-ax3 = fig3.add_subplot(111)
-ax3.scatter(time_evol_cav,thermpr_evol_cav,s=1,color='red',label='partially-confined')
-ax3.scatter(time_evol_ff,thermpr_evol_ff,s=1,color='blue',label='free-field')
-ax3.set_xlabel('time [s]')
-ax3.set_ylabel('therm pr [kg m^-1 s^-2]')
+ax3.set_xlabel('time [Myr]')
+ax3.set_ylabel('ram pressure [g $\mathrm{cm^{-1}}$ $\mathrm{s^{-2}}$]')
 ax3.set_yscale('log')
-ax3.set_ylim([2E-13,6E-6])
-ax3.legend()
-fig3.savefig('sim_thermp.png',dpi=200)
+ax3.set_xlim([0,3.1])
+ax3.set_ylim([1E-19,5E-3])
+ax3.legend(loc='upper right',fontsize=8)
 
-fig4 = plt.figure(figsize=[7,5])
-ax4 = fig4.add_subplot(111)
-ax4.scatter(time_evol_cav,rampr_evol_cav,s=1,color='red',label='partially-confined')
-ax4.scatter(time_evol_ff,rampr_evol_ff,s=1,color='blue',label='free-field')
-ax4.set_xlabel('time [s]')
-ax4.set_ylabel('ram pr [kg m^-1 s^-2]')
-ax4.set_yscale('log')
-ax4.set_ylim([3E-15,5E-5])
-ax4.legend()
-fig4.savefig('sim_ramp.png',dpi=200)
+fig.tight_layout(pad=1.0)
+
+plt.savefig('gas_prop_detector.png')
 
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
